4a52234723dd773e2f3180971040abf5270422de
Sebastian-Jurek <sebastian.jurek44@gmail.com>
2021-11-17 20:14:32 +0100

    added hamburger menu for mobile

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
 const menuBtn = document.querySelector(".menu-btn");
 const navLinksMobile = document.querySelector(".nav-links-mobile");
+const bodyOverflow = document.querySelector("body");
+
 let menuOpen = false;
 menuBtn.addEventListener("click", () => {
   if (!menuOpen) {
     menuBtn.classList.add("open");
     navLinksMobile.classList.add("open");
+    bodyOverflow.style.overflow = "hidden";
     menuOpen = true;
   } else {
     menuBtn.classList.remove("open");
     navLinksMobile.classList.remove("open");
+    bodyOverflow.style.overflow = "auto";
     menuOpen = false;
   }
 });
