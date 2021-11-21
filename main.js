5a4105bb418ceb2a0c3bf4d6b7939fc92e7b236d
Sebastian-Jurek <sebastian.jurek44@gmail.com>
2021-11-21 18:22:09 +0100

    changed html structure and separate css files for all pages

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const menuBtn = document.querySelector(".menu-btn");
 const navLinksMobile = document.querySelector(".nav-links-mobile");
 const bodyOverflow = document.querySelector("body");
+const logoSmall = document.querySelector(".nav-logo-small");
 
 let menuOpen = false;
 menuBtn.addEventListener("click", () => {
@@ -8,11 +9,13 @@ menuBtn.addEventListener("click", () => {
     menuBtn.classList.add("open");
     navLinksMobile.classList.add("open");
     bodyOverflow.style.overflow = "hidden";
+    logoSmall.style.display = "none";
     menuOpen = true;
   } else {
     menuBtn.classList.remove("open");
     navLinksMobile.classList.remove("open");
     bodyOverflow.style.overflow = "auto";
+    logoSmall.style.display = "block";
     menuOpen = false;
   }
 });
