1e97d69faf9683066b97f8c46b03414dec450c81
Sebastian-Jurek <sebastian.jurek44@gmail.com>
2022-01-06 13:45:38 +0100

    link to in-post map (when paczkomat selected) has been added

--- order/shipping.js
+++ order/shipping.js
@@ -3,13 +3,17 @@ shippingOptions.addEventListener("click", function () {
   shippingOptions.style = "";
 });
 
+const paczkomatMap = document.querySelector("#paczkomatMap");
+
 shippingOptions.addEventListener("change", (event) => {
   const selectedPaczkomat = document.querySelector("#paczkomat");
   if (event.target.value == 8.99) {
     selectedPaczkomat.style = "display: block";
+    paczkomatMap.style = "display:block";
     selectedPaczkomat.required = true;
   } else {
     selectedPaczkomat.style = "display: none";
+    paczkomatMap.style = "display: none";
     selectedPaczkomat.required = false;
   }
 });
